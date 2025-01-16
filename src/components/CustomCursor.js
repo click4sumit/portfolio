5c666ea32db2a153987d0409cdf74da95e6f373c
sumit <libscienceworld@gmail.com>
2025-01-16 23:08:05 +0530

    project setup

--- src/components/CustomCursor.js
+++ src/components/CustomCursor.js
@@ -38,3 +38,66 @@ const CustomCursor = () => {
 
 export default CustomCursor
 
+
+
+// 'use client'
+
+// import { useState, useEffect, useCallback } from 'react'
+// import { motion, useSpring, useMotionValue } from 'framer-motion'
+
+// const CustomCursor = () => {
+//   const cursorX = useMotionValue(0)
+//   const cursorY = useMotionValue(0)
+
+//   const springConfig = { damping: 25, stiffness: 700 }
+//   const cursorXSpring = useSpring(cursorX, springConfig)
+//   const cursorYSpring = useSpring(cursorY, springConfig)
+
+//   const [isHovered, setIsHovered] = useState(false)
+//   const [isOverText, setIsOverText] = useState(false)
+
+//   const updateCursorPosition = useCallback((e) => {
+//     cursorX.set(e.clientX)
+//     cursorY.set(e.clientY)
+//   }, [cursorX, cursorY])
+
+//   const updateCursorState = useCallback((e) => {
+//     const target = e.target
+//     setIsHovered(target.tagName.toLowerCase() === 'a' || target.tagName.toLowerCase() === 'button' || target.closest('a') || target.closest('button'))
+    
+//     const isText = ['P', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'SPAN', 'LI'].includes(target.tagName)
+//     setIsOverText(isText)
+
+//     if (isText) {
+//       target.style.transition = 'transform 5s cubic-bezier(0.34, 1.56, 0.64, 1)'
+//       target.style.transform = 'scale(1.1)'
+//       target.style.transformOrigin = `${e.clientX}px ${e.clientY}px`
+//     } else {
+//       document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, li').forEach((el) => {
+//         (el).style.transform = 'scale(1)'
+//       })
+//     }
+//   }, [])
+
+//   useEffect(() => {
+//     window.addEventListener('mousemove', updateCursorPosition)
+//     window.addEventListener('mouseover', updateCursorState)
+
+//     return () => {
+//       window.removeEventListener('mousemove', updateCursorPosition)
+//       window.removeEventListener('mouseover', updateCursorState)
+//     }
+//   }, [updateCursorPosition, updateCursorState])
+
+//   return (
+//     <motion.div 
+//       className={`custom-cursor ${isHovered ? 'hover' : ''} ${isOverText ? 'over-text' : ''}`}
+//       style={{
+//         left: cursorXSpring,
+//         top: cursorYSpring,
+//       }}
+//     />
+//   )
+// }
+
+// export default CustomCursor;
