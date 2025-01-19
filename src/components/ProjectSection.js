b2f1630bc1233f5ce413b98147b5aab00e0d43b9
sumit <libscienceworld@gmail.com>
2025-01-19 16:16:16 +0530

    Project setup

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -5,21 +5,21 @@ export default function ProjectSection() {
 
     const projects = [ 
         {
-            title: "E-commerce Platform",
+            title: "Dummy Apple Website",
             link: "https://dumywebsite.vercel.app/", 
-            description: "A full-stack e-commerce solution with React and Node.js",
+            description: "A fully responsive website based on Nextjs",
             image: "https://images.unsplash.com/photo-1517292987719-0369a794ec0f?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            title: "Task Management App",
+            title: "School Website",
             link: "https://school-website-alpha-sooty.vercel.app/",
-            description: "A React Native mobile app for managing daily tasks",
+            description: "A school website with react, node, mongodb",
             image: "https://images.unsplash.com/photo-1736131660777-8b7aa6bb0efe?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
         {
-            title: "Weather Dashboard",
-            link: "https://dumywebsite.vercel.app/",
-            description: "A real-time weather dashboard using OpenWeatherMap API",
+            title: "Fully Responsive Website",
+            link: "https://responsive-website-pied-psi.vercel.app/",
+            description: "Website based on Mern stack ",
             image: "https://images.unsplash.com/photo-1713427607338-8edc529eeaa4?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           },
 
