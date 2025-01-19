b40a309fe0754cdf13d17fcdda454cf982e18882
sumit <libscienceworld@gmail.com>
2025-01-19 15:09:08 +0530

    Project setup

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -12,7 +12,7 @@ export default function ProjectSection() {
         },
         {
             title: "Task Management App",
-            link: "https://dumywebsite.vercel.app/",
+            link: "https://school-website-alpha-sooty.vercel.app/",
             description: "A React Native mobile app for managing daily tasks",
             image: "https://images.unsplash.com/photo-1736131660777-8b7aa6bb0efe?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
